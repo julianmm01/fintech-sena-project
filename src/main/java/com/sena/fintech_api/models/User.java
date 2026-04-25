d86693aa3acf8d5f5f8b1a258c3d2fbae6eed103
julianmm01 <julianmoraless3021@gmail.com>
2026-04-24 19:23:35 -0500

    feat: configurar entorno en Medellín y crear entidad User

---
+++ src/main/java/com/sena/fintech_api/models/User.java
@@ -0,0 +1,16 @@
+package com.sena.fintech_api.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
